9eef8084738422054e7d4f93cc8d1628b756cd8c
Aditya <imaditya05@gmail.com>
2023-04-07 15:00:41 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#202123',
+        'light-gray': '#343541',
+      },
+    },
+  },
+  plugins: [],
+};
